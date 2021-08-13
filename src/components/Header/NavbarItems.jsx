94a4d39b851fe54e82c20ce5de82dd0a2e928f70
Hắc Tún <hactuns@MacBook-Pro-cua-Hac.local>
2021-08-13 13:01:54 +0700

    inline block nav link

--- src/components/Header/NavbarItems.jsx
+++ src/components/Header/NavbarItems.jsx
@@ -8,7 +8,7 @@ function NavbarItems({ category, className = "" }) {
     <Nav className={className}>
       {category.map((item, index) => (
         <NavItem key={index} className="text-center">
-          <Link to={`/${item === "Home" ? "" : item}`} className="nav-link font-weight-bold my-1" style={{ textTransform: "capitalize", fontSize: "1.25rem" }}>
+          <Link to={`/${item === "Home" ? "" : item}`} className="nav-link font-weight-bold my-1 d-inline-block" style={{ textTransform: "capitalize", fontSize: "1.25rem" }}>
             {item}
           </Link>
         </NavItem>
